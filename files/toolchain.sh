a3d6a8bd62d3b31325e7a9581374733e0183a482
Minsoo Kim <leios@falinux.com>
2017-07-10 19:05:21 +0900

    파일 이름 변경

--- files/toolchain.sh
+++ files/toolchain.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd /work && \
-  source env-4.9.4.env && \
+  source package.env && \
   cd buildroot/${BUILDROOT_DIR} && \
   echo " [*] Building toolchain..." && \
   ./make.sh toolchain 1>/dev/null 2>/dev/null && \
   ./make.sh 1>/dev/null 2>/dev/null && \
   cd /opt && \
   echo " [*] Packaging toolchain..." && \
-  tar czf /work/docker/toolchain/arm-buildroot-linux-gnueabihf-4.9.4.tar.gz . 1>/dev/null 2>/dev/null&& \
+  tar czf /work/docker/toolchain/arm-buildroot-linux-gnueabihf.tar.gz . 1>/dev/null 2>/dev/null&& \
   cd - 1>/dev/null 2>/dev/null && \
   echo " [*] Cleaning..." && \
   ./make.sh distclean 1>/dev/null 2>/dev/null
